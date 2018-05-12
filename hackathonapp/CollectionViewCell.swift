fd873eae025f1f351fbc028af5e0272ac40c7732
BrendanWu <wubrendan7@gmail.com>
2018-05-12 03:31:31 -0400

    login/signup + firebase scroll UICollectionView

---
+++ hackathonapp/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  hackathonapp
+//
+//  Created by Brendan Wu on 2018-05-12.
+//  Copyright © 2018 Brendan Wu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var locationImage: UIImageView!
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var locationDescription: UILabel!
+}
--- hackathonapp/HomeViewController.swift
+++ hackathonapp/HomeViewController.swift
@@ -11,14 +11,46 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func handleLogout(_ target: UIBarButtonItem) {
-            try! Auth.auth().signOut()
-            
         
     }
+    @IBAction func handleLogout(_ target: UIBarButtonItem) {
+        try! Auth.auth().signOut()
+    }
+    let locationName = ["Canada's Wonderland", "Theme Park", "Rollercoasters"]
+    let locationImage = [UIImage(named: "wonderland"),UIImage(named: "bahamas"),UIImage(named: "maxresdefault")]
+    
+    let locationDescription = ["Best theme park in North America!", "An exhilirating watery retreat", "An amazing exploration event in Hawaii"]
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return locationName.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.locationName.text = locationName[indexPath.row]
+        cell.locationImage.image = locationImage[indexPath.row]
+        cell.locationDescription.text = locationDescription[indexPath.row]
+        //
+        cell.contentView.layer.cornerRadius = 4.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        
+        return cell
+    }
+    
+    
 }
